9f3d2122ca12d6a99902c16d14929bba0d814275
andressacoelhoo <andressa.coelho.viana@escola.pr.gov.br>
2025-08-20 10:24:29 -0300

    im

--- main.js
+++ main.js
@@ -1,16 +1,41 @@
-<html lang="pt-br">
-<body>
-    <h1> Efeitos som </h1>
-    <h2> Aberturas de Aniime</h2>
+function tocaSom(idElementoAudio){
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+       
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function(evento){
+     
+        if (evento.code === 'Space'|| evento.code ==='Enter'){
+            tecla.classList.add('ativa');
+        }
     
+}
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+    
+}
 
 
 
 
 
 
-</body>
 
 
 
-</html>
+
